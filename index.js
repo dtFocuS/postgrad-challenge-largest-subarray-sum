5ca6bf92062f78594da907672aae875f2c80293c
Danny <dtfocus1025@gmail.com>
2020-02-23 14:45:27 -0800

    added time and space complexity

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function largestSubarraySum(array){
-
+    //Time Complexity is O(N) where N is the length of the array since there is only one for loop that iterates through every single element in the array
+    //Space Complexity is O(1) which is constant because I didn't use any other data structure to solve this problem other than a couple of variables to hold the current sum and the max sum.
     if (!array || array.length === 0) {
         return 0;
     }
